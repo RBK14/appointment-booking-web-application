9baea64f3d08b93d1c169ac02be05dc5d5f98b6b
RBK14 <maciek.rybka2004@gmail.com>
2025-04-27 01:48:57 +0200

    Minor changes in back-end configuration

--- appointment-booking/src/main/java/com/example/appointmentbooking/AppointmentBookingApplication.java
+++ appointment-booking/src/main/java/com/example/appointmentbooking/AppointmentBookingApplication.java
@@ -1,4 +1,4 @@
-package com.example.appointment_booking;
+package com.example.appointmentbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
